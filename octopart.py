cda31dc70e12f7b4a16634d849f5b4ecadfc7d53
niney <kpeteras@gmail.com>
2021-09-26 11:08:03 +0900

    closes #4 octopart 통합 개선 및 작업들

---
+++ octopart.py
@@ -0,0 +1,469 @@
+import json
+import logging
+import random
+
+from functools import cmp_to_key
+
+
+class ClientHelper:
+    samplepcb_auth_provider = ['mouser', 'digi-key', 'element14-apac']
+
+    def part_sort(self, x, y):
+        x_company = x['company']
+        y_company = y['company']
+        if len(x_company) == 0 and len(y_company) != 0:
+            return 1  # y_company 앞으로
+        if len(y_company) == 0 and len(x_company) != 0:
+            return -1  # x_company 앞으로
+        if len(x_company) != 0 and len(y_company) != 0:
+            if y_company['slug'] in self.samplepcb_auth_provider:
+                return 1
+            if x_company['slug'] in self.samplepcb_auth_provider:
+                return -1
+
+        x_offer = x['offers']
+        y_offer = y['offers']
+        if len(x_offer) == 0 and len(y_offer) == 0:
+            return 0
+        if len(x_offer) == 0 and len(y_offer) != 0:
+            return 1  # y_offer 앞으로
+        if len(y_offer) == 0 and len(x_offer) != 0:
+            return -1  # x_offer 앞으로
+
+        if x_offer['moq'] == 1 and y_offer['moq'] == 1:
+            x_price = -1
+            y_price = -1
+            for price_info in x_offer['prices']:
+                if price_info['quantity'] == 1:
+                    x_price = price_info['price']
+
+            for price_info in y_offer['prices']:
+                if price_info['quantity'] == 1:
+                    y_price = price_info['price']
+
+            # 1개의 가격정보 여부 체크
+            if x_price == -1 and y_offer['inventory_level'] > 0:
+                return 1  # y_offer 앞으로
+
+            if y_price == -1 and x_offer['inventory_level'] > 0:
+                return -1  # x_offer 앞으로
+
+            # 싼가격 체크
+            if x_price < y_price and y_offer['inventory_level'] > 0:
+                return -1  # x_offer 앞으로
+            elif x_price > y_price and x_offer['inventory_level'] > 0:
+                return 1  # y_offer 앞으로
+
+        # moq none 검사
+        if x_offer['moq'] is None and y_offer['moq'] is None:
+            return 0
+        if x_offer['moq'] is None and y_offer['moq'] is not None:
+            return 1  # y_offer 앞으로
+        if y_offer['moq'] is None and x_offer['moq'] is not None:
+            return -1  # x_offer 앞으로
+
+        # moq 적은 수량 우선순위
+        if x_offer['moq'] > y_offer['moq']:
+            return 1  # y_offer 앞으로
+        if x_offer['moq'] < y_offer['moq']:
+            return -1  # x_offer 앞으로
+
+        if len(x_offer['prices']) == 0 and len(y_offer['prices']) > 0:
+            return 1  # y_offer 앞으로
+        if len(y_offer['prices']) == 0 and len(x_offer['prices']) > 0:
+            return -1  # x_offer 앞으로
+        if len(y_offer['prices']) == 0 and len(x_offer['prices']) == 0:
+            return 0
+
+        x_price = x_offer['prices'][0]['price']
+        y_price = y_offer['prices'][0]['price']
+
+        # 재고체크
+        if x_price < y_price and y_offer['inventory_level'] > 0:
+            return 1  # y_offer 앞으로
+        elif x_price > y_price and x_offer['inventory_level'] > 0:
+            return -1  # x_offer 앞으로
+
+        return 0
+
+    def setting_lowest_price(self, part):
+        for i, seller in enumerate(part['sellers']):
+            cut_tape_offer = None
+            for offer in seller['offers']:
+                if offer['packaging'] == 'Cut Tape':
+                    cut_tape_offer = offer
+
+            if cut_tape_offer is None:
+                offers_list = part['sellers'][i]['offers']
+                if len(offers_list) == 1:
+                    random_idx = 0
+                else:
+                    random_idx = random.randrange(0, len(offers_list) - 1)
+                part['sellers'][i]['offers'] = part['sellers'][i]['offers'][random_idx]
+            else:
+                part['sellers'][i]['offers'] = cut_tape_offer
+        searched_sellers = sorted(part['sellers'], key=cmp_to_key(self.part_sort))
+        if len(searched_sellers) > 0:
+            part['sellers'] = searched_sellers[0]
+
+    def setting_margin(self, part):
+        if 'sellers' in part:
+            searched_seller = part['sellers']
+            self.setting_margin_by_seller(searched_seller)
+
+        if 'sellersAll' in part:
+            for seller in part['sellersAll']:
+                self.setting_margin_by_seller(seller)
+
+    def setting_margin_by_seller(self, seller):
+        if 'offers' in seller and 'prices' in seller['offers']:
+            for price in seller['offers']['prices']:
+                price['converted_price'] = price['converted_price'] + (price['converted_price'] * 0.095)
+
+    def get_parts_query(self):
+        """
+        octopart parts 쿼리 가져오기
+        :return:
+        """
+        return '''
+            id
+            name
+            short_description
+            category {
+              id
+              name
+              path
+            }
+            descriptions {
+              text
+              credit_string
+              credit_url
+            }
+            specs {
+              attribute {
+                name
+                shortname
+                group
+              }
+              display_value
+            }
+            slug
+            series {
+              name
+            }
+            manufacturer {
+              name
+            }
+            mpn
+            generic_mpn
+            best_image {
+              url
+              credit_url
+              credit_string
+            }
+            best_datasheet {
+              name
+              url
+              created_at
+            }
+            images {
+              url
+            }
+            reference_designs {
+              name
+              url
+            }
+            sellers(
+              authorized_only: true
+            ) {
+              company {
+                name
+                aliases
+                homepage_url
+                slug
+              }
+              country
+              offers {
+                sku
+                inventory_level
+                packaging
+                moq
+                click_url
+                # internal_url
+                factory_lead_days
+                factory_pack_quantity
+                on_order_quantity
+                multipack_quantity
+                prices {
+                  quantity
+                  price
+                  currency
+                  converted_price
+                  converted_currency
+                  conversion_rate
+                }
+                order_multiple
+                updated
+                is_custom_pricing
+              }
+            }
+        '''
+
+    def get_parts(self, client, q, page=1, param=None):
+        filter_param_str = ''
+        q_str = ''
+        categories_str = ''
+        if q is not None:
+            q_str = 'q: "' + q + '"\n'
+
+        page_str = 'start: ' + str((int(page) - 1) * 10) + '\n'
+
+        if param is not None:
+            if 'manufacturer_id' in param and len(param['manufacturer_id']) != 0:
+                filter_param_str += 'manufacturer_id : [' + ','.join(param['manufacturer_id']) + ']\n'
+            # if 'mount' in param and len(param['mount']) != 0:
+            #     filter_param_str += 'mount : ["' + '","'.join(param['mount']) + '"]\n'
+            # if 'case_package' in param and len(param['case_package']) != 0:
+            #     filter_param_str += 'case_package : ["' + '","'.join(param['case_package']) + '"]\n'
+            # if 'tolerance' in param and len(param['tolerance']) != 0:
+            #     filter_param_str += 'tolerance : ' + str(param['tolerance']) + '\n'
+            # if 'powerrating' in param and len(param['powerrating']) != 0:
+            #     filter_param_str += 'powerrating : ' + str(param['powerrating']) + '\n'
+            # if 'voltagerating_dc_' in param and len(param['voltagerating_dc_']) != 0:
+            #     filter_param_str += 'voltagerating_dc_ : ' + str(param['voltagerating_dc_']) + '\n'
+            if 'filters' in param:
+                for f_key, f_val in param['filters'].items():
+                    if len(f_val) != 0:
+                        filter_param_str += f'{f_key} : ["' + '","'.join(f_val) + '"]\n'
+            if 'categories' in param:
+                if 'ids' in param['categories']:
+                    categories_str += 'ids : ["' + '","'.join(param['categories']['ids']) + '"]\n'
+                    filter_param_str += 'category_id : ["' + '","'.join(param['categories']['ids']) + '"]\n'
+
+        filter_str = ''
+        if filter_param_str is not '':
+            filter_str = """
+            filters: {
+              """ + filter_param_str + """ 
+            }
+            """
+
+        if categories_str is not '':
+            categories_str = """
+                categories(
+                    """ + categories_str + """
+                  ) {
+                    id
+                    name
+                    path
+                    parent_id
+                    ancestors {
+                      id
+                      name
+                      children {
+                        id
+                        name
+                      }
+                    }
+                    children {
+                      id
+                      name
+                      path
+                      children {
+                        id
+                        name
+                        path
+                      }
+                    }
+                  }
+            """
+
+        spec_aggs_names_str = ''
+        if param is not None and 'specAggsNames' in param:
+            spec_aggs_names_str = " ".join(param['specAggsNames'])
+
+        query = '''
+            query partSearch {
+            
+              ''' + categories_str + '''
+            
+              search(
+                ''' + q_str + '''
+                country: "KR"
+                currency: "KRW"
+                ''' + page_str + '''
+                limit: 10
+                ''' + filter_str + '''
+                
+                ) {
+                
+                hits
+        
+                manufacturer_agg {
+                  company {
+                    id
+                    name
+                  }
+                  count
+                }
+                
+                spec_aggs(
+                  attribute_names: "'''+ spec_aggs_names_str+'''"
+                  size: 255
+                ) {
+                  attribute {
+                    id
+                    name
+                    shortname
+                    group
+                  }
+                  buckets {
+                    display_value
+                    float_value
+                    count
+                  }
+                }
+                
+                category_agg {
+                  category {
+                    parent_id
+                    id
+                    name
+                    path
+                  }
+                  count
+                }
+                        
+                results {
+                  description
+                  part {
+                  ''' + self.get_parts_query() + '''
+                  }
+                }
+              }
+            }
+        '''
+
+        resp = client.execute(query)
+        return json.loads(resp)['data']
+
+    def get_parts_by_ids(self, client, param=None):
+
+        query = '''
+            query partIdSearch($ids: [String!]!) {
+                parts(
+                    ids: $ids
+                    country: "KR"
+                    currency: "KRW"
+                ) {
+                    ''' + self.get_parts_query() + '''
+                }
+            }
+        '''
+        resp = client.execute(query, param)
+        return json.loads(resp)['data']
+
+    def get_categories(self, client, param=None):
+
+        categories_str = ''
+        if 'categories' in param:
+            if 'ids' in param['categories']:
+                categories_str += 'ids : ["' + '","'.join(param['categories']['ids']) + '"]\n'
+
+        categories_str = """
+            categories(
+                """ + categories_str + """
+              ) {
+                id
+                name
+                path
+                parent_id
+                ancestors {
+                  id
+                  name
+                  children {
+                    id
+                    name
+                  }
+                }
+                children {
+                  id
+                  name
+                  path
+                  children {
+                    id
+                    name
+                    path
+                  }
+                }
+              }
+        """
+
+        query = '''
+            query partSearch {
+              ''' + categories_str + '''
+            }
+        '''
+
+        resp = client.execute(query)
+        return json.loads(resp)['data']
+
+    def get_manufacturers(self, client, param=None):
+        manufacturers_str = '''
+            manufacturers {
+              id
+              name
+              aliases
+              display_flag
+              homepage_url
+              slug
+              is_verified
+              is_distributorapi
+            }
+        '''
+
+        query = '''
+            query partSearch {
+              ''' + manufacturers_str + '''
+            }
+        '''
+
+        resp = client.execute(query)
+        return json.loads(resp)['data']
+
+    def get_suggested_filters(self, client, q, param=None):
+
+        q_str = ''
+        if q is not None:
+            q_str = 'q: "' + q + '"\n'
+
+        filter_param_str = ''
+        if param is not None and 'categories' in param:
+            if 'ids' in param['categories']:
+                filter_param_str += 'category_id : ["' + '","'.join(param['categories']['ids']) + '"]\n'
+
+        filter_str = ''
+        if filter_param_str is not '':
+            filter_str = """
+            filters: {
+              """ + filter_param_str + """ 
+            }
+            """
+
+        query = '''
+          query suggestedSearch {
+            search(
+               ''' + q_str + '''
+               ''' + filter_str + '''
+            ) {
+              suggested_filters {
+                id
+                name
+                shortname
+                group
+              }
+            } 
+          }
+        '''
+        resp = client.execute(query)
+        return json.loads(resp)['data']
--- samplepcb_ml_prototype.py
+++ samplepcb_ml_prototype.py
@@ -5,11 +5,11 @@
 
 import json
 import logging
-import random
 from logging import handlers
 
 import column_analysis as colanal
 import graph_ql_client
+import octopart
 
 logger = None
 
@@ -71,221 +71,6 @@ def get_parts2(client, ids):
     return json.loads(resp)['data']['parts']
 
 
-def get_parts(client, q, param):
-
-    param_str = ''
-    if param is not None:
-        if len(param['manufacturer_id']) != 0:
-            param_str += 'manufacturer_id : [' + ','.join(param['manufacturer_id']) + ']\n'
-        if len(param['mount']) != 0:
-            param_str += 'mount : ["' + '","'.join(param['mount']) + '"]\n'
-        if len(param['case_package']) != 0:
-            param_str += 'case_package : ["' + '","'.join(param['case_package']) + '"]\n'
-        if len(param['tolerance']) != 0:
-            param_str += 'tolerance : ' + str(param['tolerance']) + '\n'
-        if len(param['powerrating']) != 0:
-            param_str += 'powerrating : ' + str(param['powerrating']) + '\n'
-        if len(param['voltagerating_dc_']) != 0:
-            param_str += 'voltagerating_dc_ : ' + str(param['voltagerating_dc_']) + '\n'
-
-    filter_str = ''
-    if param_str is not '':
-        filter_str = """
-        filters: {
-          """ + param_str + """ 
-        }
-        """
-
-    query = '''
-    query partSearch($q: String) {
-      search(
-        q: $q
-        country: "KR"
-        currency: "KRW"
-        limit: 3
-        ''' + filter_str + '''
-        
-        ) {
-        
-        hits
-
-        manufacturer_agg {
-          company {
-            id
-            name
-          }
-          count
-        }
-        
-        spec_aggs(
-          attribute_names: "mount case_package tolerance powerrating voltagerating_dc_"
-        ) {
-          attribute {
-            id
-            name
-            shortname
-            group
-          }
-          buckets {
-            display_value
-            float_value
-            count
-          }
-        }
-                
-        results {
-          description
-          part {
-            id
-            name
-            short_description
-            category {
-              id
-              name
-              path
-            }
-            descriptions {
-              text
-              credit_string
-              credit_url
-            }
-            specs {
-              attribute {
-                name
-                shortname
-                group
-              }
-              display_value
-            }
-            slug
-            series {
-              name
-            }
-            manufacturer {
-              name
-            }
-            mpn
-            generic_mpn
-            best_image {
-              url
-              credit_url
-              credit_string
-            }
-            images {
-              url
-            }
-            reference_designs {
-              name
-              url
-            }
-            sellers {
-              company {
-                name
-                aliases
-                homepage_url
-                slug
-              }
-              country
-              offers {
-                sku
-                inventory_level
-                packaging
-                moq
-                click_url
-                # internal_url
-                factory_lead_days
-                factory_pack_quantity
-                on_order_quantity
-                multipack_quantity
-                prices {
-                  quantity
-                  price
-                  currency
-                  converted_price
-                  converted_currency
-                  conversion_rate
-                }
-                order_multiple
-                updated
-                is_custom_pricing
-              }
-            }
-          }
-        }
-      }
-    }
-    '''
-
-    resp = client.execute(query, {'q': q})
-    return json.loads(resp)['data']
-
-
-def part_sort(x, y):
-    x_offer = x['offers']
-    y_offer = y['offers']
-    if len(x_offer) == 0 and len(y_offer) == 0:
-        return 0
-    if len(x_offer) == 0 and len(y_offer) != 0:
-        return 1  # y_offer 앞으로
-    if len(y_offer) == 0 and len(x_offer) != 0:
-        return -1  # x_offer 앞으로
-
-    if x_offer['moq'] == 1 and y_offer['moq'] == 1:
-        x_price = -1
-        y_price = -1
-        for price_info in x_offer['prices']:
-            if price_info['quantity'] == 1:
-                x_price = price_info['price']
-
-        for price_info in y_offer['prices']:
-            if price_info['quantity'] == 1:
-                y_price = price_info['price']
-
-        # 1개의 가격정보 여부 체크
-        if x_price == -1 and y_offer['inventory_level'] > 0:
-            return 1  # y_offer 앞으로
-
-        if y_price == -1 and x_offer['inventory_level'] > 0:
-            return -1  # x_offer 앞으로
-
-        # 싼가격 체크
-        if x_price < y_price and y_offer['inventory_level'] > 0:
-            return 1  # y_offer 앞으로
-        elif x_price > y_price and x_offer['inventory_level'] > 0:
-            return -1  # x_offer 앞으로
-
-    # moq none 검사
-    if x_offer['moq'] is None and y_offer['moq'] is None:
-        return 0
-    if x_offer['moq'] is None and y_offer['moq'] is not None:
-        return 1  # y_offer 앞으로
-    if y_offer['moq'] is None and x_offer['moq'] is not None:
-        return -1  # x_offer 앞으로
-
-    # moq 적은 수량 우선순위
-    if x_offer['moq'] > y_offer['moq']:
-        return 1  # y_offer 앞으로
-    if x_offer['moq'] < y_offer['moq']:
-        return -1  # x_offer 앞으로
-
-    if len(x_offer['prices']) == 0 and len(y_offer['prices']) > 0:
-        return 1  # y_offer 앞으로
-    if len(y_offer['prices']) == 0 and len(x_offer['prices']) > 0:
-        return -1  # x_offer 앞으로
-    if len(y_offer['prices']) == 0 and len(x_offer['prices']) == 0:
-        return 0
-
-    x_price = x_offer['prices'][0]['price']
-    y_price = y_offer['prices'][0]['price']
-
-    # 재고체크
-    if x_price < y_price and y_offer['inventory_level'] > 0:
-        return 1  # y_offer 앞으로
-    elif x_price > y_price and x_offer['inventory_level'] > 0:
-        return -1  # x_offer 앞으로
-
-    return 0
-
 ##### MAIN SCRIPT #####
 
 
@@ -297,7 +82,6 @@ from flask import Flask, jsonify
 from flask import request
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
-from functools import cmp_to_key
 
 app = Flask(__name__)
 
@@ -341,30 +125,87 @@ def search_column():
 def search_parts():
     client = graph_ql_client.GraphQLClient('https://octopart.com/api/v4/endpoint')
     client.inject_token(app.config['OCTOPART_APIKEY'])
-    parts = get_parts(client, request.values['q'], request.get_json())
+    client_helper = octopart.ClientHelper()
+    page = 1
+    if 'page' in request.values:
+        page = request.values['page']
+    if 'q' in request.values:
+        parts = client_helper.get_parts(client, request.values['q'], page, request.get_json())
+    else:
+        parts = client_helper.get_parts(client, None, page, request.get_json())
+
     if parts['search']['hits'] > 0:
-        for result in parts['search']['results']:
-            for i, seller in enumerate(result['part']['sellers']):
-                cut_tape_offer = None
-                for offer in seller['offers']:
-                    if offer['packaging'] == 'Cut Tape':
-                        cut_tape_offer = offer
-
-                if cut_tape_offer is None:
-                    offers_list = result['part']['sellers'][i]['offers']
-                    if len(offers_list) == 1:
-                        random_idx = 0
-                    else:
-                        random_idx = random.randrange(0, len(offers_list) - 1)
-                    result['part']['sellers'][i]['offers'] = result['part']['sellers'][i]['offers'][random_idx]
-                else:
-                    result['part']['sellers'][i]['offers'] = cut_tape_offer
-
-            searched_sellers = sorted(result['part']['sellers'], key=cmp_to_key(part_sort))
-            if len(searched_sellers) > 0:
-                result['part']['sellers'] = searched_sellers[0]
+        if parts['search']['results'] is not None:
+            for result in parts['search']['results']:
+                part = result['part']
+                if 'sellersAll' in request.values:
+                    part['sellersAll'] = part['sellers']
+                client_helper.setting_lowest_price(part)
+                client_helper.setting_margin(part)
 
     # print(parts.results)
+    return jsonify(parts)
+
+
+@app.route('/api/searchCategories', methods=['GET', 'POST'])
+def search_categories():
+    client = graph_ql_client.GraphQLClient('https://octopart.com/api/v4/endpoint')
+    client.inject_token(app.config['OCTOPART_APIKEY'])
+
+    client_helper = octopart.ClientHelper()
+    parts = client_helper.get_categories(client, request.get_json())
+
+    return jsonify(parts)
+
+
+@app.route('/api/searchPartsByIds', methods=['GET', 'POST'])
+def search_parts_by_ids():
+    client = graph_ql_client.GraphQLClient('https://octopart.com/api/v4/endpoint')
+    client.inject_token(app.config['OCTOPART_APIKEY'])
+    client_helper = octopart.ClientHelper()
+    resp = client_helper.get_parts_by_ids(client, request.get_json())
+
+    if len(resp['parts']) > 0:
+        for part in resp['parts']:
+            if 'sellersAll' in request.values:
+                part['sellersAll'] = part['sellers']
+            client_helper.setting_lowest_price(part)
+            client_helper.setting_margin(part)
+    return jsonify(resp)
+
+
+@app.route('/api/v1/searchQuery', methods=['GET', 'POST'])
+def search_query():
+    client = graph_ql_client.GraphQLClient('https://octopart.com/api/v4/endpoint')
+    client.inject_token(app.config['OCTOPART_APIKEY'])
+    resp = client.execute(request.get_json()['query'], request.get_json()['variables'])
+    return json.loads(resp)['data']
+
+
+@app.route('/api/searchManufacturers', methods=['GET', 'POST'])
+def search_manufacturers():
+    client = graph_ql_client.GraphQLClient('https://octopart.com/api/v4/endpoint')
+    client.inject_token(app.config['OCTOPART_APIKEY'])
+
+    client_helper = octopart.ClientHelper()
+    parts = client_helper.get_manufacturers(client, request.get_json())
+
+    return jsonify(parts)
+
+
+@app.route('/api/searchSuggested', methods=['GET', 'POST'])
+def search_suggested():
+    client = graph_ql_client.GraphQLClient('https://octopart.com/api/v4/endpoint')
+    client.inject_token(app.config['OCTOPART_APIKEY'])
+    client_helper = octopart.ClientHelper()
+    if len(request.data) == 0:
+        json_data = None
+    else:
+        json_data = request.get_json()
+    if 'q' in request.values:
+        parts = client_helper.get_suggested_filters(client, request.values['q'], json_data)
+    else:
+        parts = client_helper.get_suggested_filters(client, None, json_data)
     return jsonify(parts)
 
 
